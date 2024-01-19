f16aa0fcea15659547c2ade581d1f652b495e62c
Apolo Moraes <vlogsapolo@gmail.com>
2024-01-19 18:27:51 -0300

    added grid style HomePage page

--- src/containers/HomePage/styles.tsx
+++ src/containers/HomePage/styles.tsx
@@ -1,4 +1,8 @@
 /* eslint-disable prettier/prettier */
 import styled from 'styled-components';
 
-export const Container = styled.main``;
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+  gap: 3rem;
+`;
